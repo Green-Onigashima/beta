cc7c9af8a4c6c722beb5210a78acfd3e96f34cab
Green-Onigashima <greenmorongetlostagain@gmail.com>
2024-07-25 14:32:50 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -1,15 +1,19 @@
-#(©)Codexbotz
 
 import base64
 import re
+import requests
+import time
 import asyncio
 from pyrogram import filters
 from pyrogram.enums import ChatMemberStatus
-from config import ADMINS
+from config import *
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 from pyrogram.errors import FloodWait
+from shortzy import Shortzy
+from datetime import datetime
+from database.database import user_data, db_verify_status, db_update_verify_status, fsub
 
-from database.database import fsub
+
 
 async def is_subscribed(filter, client, update):
     bot_id = client.me.id
@@ -117,5 +121,70 @@ def get_readable_time(seconds: int) -> str:
     up_time += ":".join(time_list)
     return up_time
 
+
+async def get_verify_status(user_id):
+    verify = await db_verify_status(user_id)
+    return verify
+
+async def update_verify_status(user_id, verify_token="", is_verified=False, verified_time=0, link=""):
+    current = await db_verify_status(user_id)
+    current['verify_token'] = verify_token
+    current['is_verified'] = is_verified
+    current['verified_time'] = verified_time
+    current['link'] = link
+    await db_update_verify_status(user_id, current)
+
+
+async def get_shortlink(url, api, link):
+    shortzy = Shortzy(api_key=api, base_site=url)
+    link = await shortzy.convert(link)
+    return link
+
+def get_exp_time(seconds):
+    periods = [('days', 86400), ('hours', 3600), ('mins', 60), ('secs', 1)]
+    result = ''
+    for period_name, period_seconds in periods:
+        if seconds >= period_seconds:
+            period_value, seconds = divmod(seconds, period_seconds)
+            result += f'{int(period_value)}{period_name}'
+    return result
+
+
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    up_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+    while count < 4:
+        count += 1
+        remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+    hmm = len(time_list)
+    for x in range(hmm):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        up_time += f"{time_list.pop()}, "
+    time_list.reverse()
+    up_time += ":".join(time_list)
+    return up_time
+
+async def increasepremtime(user_id : int, timeforprem : int):
+    if timeforprem == 0: 
+        realtime = 1
+    elif timeforprem == 1:
+        realtime = 86400*7
+    elif timeforprem == 2:
+        realtime = 86400*31
+    elif timeforprem == 3:
+        realtime == 86400*31*3
+    elif timeforprem == 4:
+        realtime == 86400*31*6
+    elif timeforprem == 5:
+        realtime == 86400*31*12
+            
+    await update_verify_status(user_id, is_verified=True, verified_time=time.time()-realtime)
 
 subscribed = filters.create(is_subscribed)
